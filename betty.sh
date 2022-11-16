e8473caaee601f24718bc830be3b8e233e871a37
mdekabs <1mawoda@gmail.com>
2022-11-16 11:57:19 +0100

    betty

--- betty.sh
+++ betty.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Simply a wrapper script to keep you from having to use betty-style
 # and betty-doc separately on every item.
-# Originally by Tim Britton 
+# Originally by Tim Britton (@wintermanc3r), multiargument added by
+# Larry Madeo (@hillmonkey)
 
 BIN_PATH="/usr/local/bin"
 BETTY_STYLE="betty-style"
