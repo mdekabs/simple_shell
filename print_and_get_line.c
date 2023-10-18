20798ac2f022abd00c78d13de4bbdf1ba28e09fd
mdekabs <1mawoda@gmail.com>
2023-10-18 11:58:21 +0100

    a function that getline and return the output

---
+++ print_and_get_line.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE -1
+#define SUCCESSFUL 0
+/**
+ * ---Algorithm____
+ * while in a loop
+ * display $ to the stdout/screen
+ * flush the buffer so that user can see the $
+ * define the char *line = Null
+ * size_t len = 0
+ * ssize_t read;
+ **/
+
+int main()
+{
+	printf("$ ");
+
+	while(TRUE)
+	{
+		fflush(stdout);
+		char *line = NULL;
+		size_t len = 0;
+		ssize_t read = getline(&line, &len, stdin);
+
+		if (read == FALSE)
+		{
+			perror("failed to read input");
+			free(line);
+			break;
+		}
+		printf("%s\n", line);
+		free(line);
+	}
+	return SUCCESSFUL;
+}
