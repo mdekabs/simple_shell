5192d09b0a2a2965ac2eaf68eb82ded4847b3b19
mdekabs <1mawoda@gmail.com>
2023-10-20 12:46:04 +0100

    wait

---
+++ wait_system_call.c
@@ -0,0 +1,34 @@
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+/**
+ * main - fork & wait function
+ * return 0;
+ */
+
+int main()
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("error processing");
+		return 1;
+	}
+	if(child_pid == 0)
+	{
+		printf("wait for me");
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+		printf("oh, its all better now\n");
+	}
+	return 0;
+}
